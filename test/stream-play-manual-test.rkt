6f2d910d178f0b3397665552c0c59581fb829f9d
David Benoit <benoid@users.noreply.github.com>
2016-10-31 12:09:42 -0400

    Update stream-play-manual-test.rkt

--- test/stream-play-manual-test.rkt
+++ test/stream-play-manual-test.rkt
@@ -4,7 +4,8 @@
 
 (provide (all-defined-out))
 
-(define sect
+(define scr
+  (score
     (section 
       (time-signature 4 4) 
       (key-signature "") 
@@ -49,42 +50,8 @@
           (note 'A 3 eighth-note)
           (note 'G 3 eighth-note)))))
 
-(define scor
-  (score sect sect sect))
 
-(define meas 
-        (measure
-          (note 'C 4 (subdivision eighth-note 3))
-          (note 'B 3 (subdivision eighth-note 3))
-          (note 'C 4 (subdivision eighth-note 3))
-          (note 'D 4 eighth-note)
-          (note 'E 4 eighth-note)
-          (note 'F 4 eighth-note)
-          (note 'G 4 eighth-note)
-          (note 'E 4 eighth-note)
-          (note 'C 4 quarter-note)))
-(define 
-  instr-part
-      (instrument-part
-        [vgame-synth-instrument 1]
-        (measure
-          (note 'C 4 eighth-note)
-          (note 'D 4 eighth-note)
-          (note 'E 4 eighth-note)
-          (note 'F 4 eighth-note)
-          (note 'G 4 eighth-note)
-          (note 'E 4 eighth-note)
-          (note 'C 4 eighth-note)
-          (note 'B 3 eighth-note))
-        (measure
-          (note 'C 4 eighth-note)
-          (note 'D 4 eighth-note)
-          (note 'E 4 eighth-note)
-          (note 'F 4 eighth-note)
-          (note 'G 4 eighth-note)
-          (note 'E 4 eighth-note)
-          (note 'C 4 quarter-note))))
-(play-score scor)
+(play-score scr)
 (display "ctl-D to quit")
 (read)
 
