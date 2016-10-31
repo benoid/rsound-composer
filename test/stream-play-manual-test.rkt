e1508498044a1f8e2b805d988a26154256a2fe09
David Benoit <ddavidrb@gmail.com>
2016-10-31 12:18:54 -0400

    update stream-play-test-manual.rkt

--- test/stream-play-manual-test.rkt
+++ test/stream-play-manual-test.rkt
@@ -46,7 +46,50 @@
           (note 'B 3 eighth-note)
           (note 'C 4 eighth-note)
           (note 'A 3 eighth-note)
-          (note 'G 3 eighth-note))))))
+          (note 'G 3 eighth-note))))
+    (section 
+      (time-signature 4 4) 
+      (key-signature "") 
+      #:tempo 120
+      (instrument-part
+        [vgame-synth-instrument 4]
+        (measure
+          (note 'C 4 eighth-note)
+          (note 'D 4 eighth-note)
+          (note 'E 4 eighth-note)
+          (note 'F 4 eighth-note)
+          (note 'G 4 eighth-note)
+          (note 'E 4 eighth-note)
+          (note 'C 4 eighth-note)
+          (note 'B 3 eighth-note))
+        (measure
+          (note 'C 4 eighth-note)
+          (note 'D 4 eighth-note)
+          (note 'E 4 eighth-note)
+          (note 'F 4 eighth-note)
+          (note 'G 4 eighth-note)
+          (note 'D 4 eighth-note)
+          (note 'E 4 quarter-note)))
+      (instrument-part
+        [vgame-synth-instrument 4]
+        (measure
+          (note 'C 4 eighth-note)
+          (note 'B 3 eighth-note)
+          (note 'C 4 eighth-note)
+          (note 'D 4 eighth-note)
+          (note 'B 3 eighth-note)
+          (note 'C 4 eighth-note)
+          (note 'A 3 eighth-note)
+          (note 'G 3 eighth-note))
+        (measure
+          (note 'C 4 eighth-note)
+          (note 'B 3 eighth-note)
+          (note 'C 4 eighth-note)
+          (note 'D 4 eighth-note)
+          (note 'B 3 eighth-note)
+          (note 'G 3 eighth-note)
+          (note 'C 4 quarter-note))))))
+
 
 
 (play-score scr)
